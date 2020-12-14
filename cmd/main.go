dc46dedf2a6f50ec17370f80d0f1fa8bcae5d756
Makhnev Andrey <ray.makhnev@gmail.com>
2020-12-14 22:24:15 +0300

    Add table and schema

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,29 @@ import (
 	"github.com/makhnev-a/todo-app-go/pkg/handler"
 	"github.com/makhnev-a/todo-app-go/pkg/repository"
 	"github.com/makhnev-a/todo-app-go/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s\n", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s\n", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
